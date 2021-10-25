e37a2aade046cf63d812257a9482c76190208104
electrovir <electrovir@users.noreply.github.com>
2021-10-25 07:33:16 -0600

    fix snowpack config for lit

--- snowpack.config.js
+++ snowpack.config.js
@@ -8,6 +8,23 @@ module.exports = {
         src: {url: '/dist'},
     },
     plugins: ['@snowpack/plugin-typescript'],
+    packageOptions: {
+        knownEntrypoints: [
+            '@lit/reactive-element',
+            '@lit/reactive-element/decorators/custom-element.js',
+            '@lit/reactive-element/decorators/event-options.js',
+            '@lit/reactive-element/decorators/property.js',
+            '@lit/reactive-element/decorators/query-all.js',
+            '@lit/reactive-element/decorators/query-assigned-nodes.js',
+            '@lit/reactive-element/decorators/query-async.js',
+            '@lit/reactive-element/decorators/query.js',
+            '@lit/reactive-element/decorators/state.js',
+            'lit-html',
+            'lit-html/async-directive.js',
+            'lit-html/directive.js',
+            'lit-html/static.js',
+        ],
+    },
     routes: [
         /* Enable an SPA Fallback in development: */
         {match: 'routes', src: '.*', dest: '/index.html'},
