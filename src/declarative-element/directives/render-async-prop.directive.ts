e3b878aa9e3cf0b7954553ae0e44bf4f67b51901
electrovir <electrovir@users.noreply.github.com>
2023-01-21 19:22:02 +0000

    allow resolutionRender to be undefined in renderAsyncProp

--- src/declarative-element/directives/render-async-prop.directive.ts
+++ src/declarative-element/directives/render-async-prop.directive.ts
@@ -10,13 +10,13 @@ export function renderAsyncProp<T>({
     asyncProp: AsyncProp<T>;
     /** This value will be rendered if the async state has not settled yet. */
     fallback: unknown;
-    resolutionRender: (resolved: UnPromise<T>) => unknown;
-    errorRender?: (error: Error) => unknown;
+    resolutionRender?: ((resolved: UnPromise<T>) => unknown) | undefined;
+    errorRender?: ((error: Error) => unknown) | undefined;
 }) {
     if (asyncProp?.error) {
         return errorRender ? errorRender(asyncProp.error) : extractErrorMessage(asyncProp.error);
     } else if (asyncProp?.resolution) {
-        return resolutionRender(asyncProp.resolution);
+        return resolutionRender ? resolutionRender(asyncProp.resolution) : asyncProp.resolution;
     } else {
         return fallback;
     }
