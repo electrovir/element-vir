9d4a45be85e18123951e865e455ab143461fba11
electrovir <electrovir@users.noreply.github.com>
2023-03-13 22:00:48 +0000

    update deps and configs

--- configs/web-test-runner.config.mjs
+++ configs/web-test-runner.config.mjs
@@ -1,9 +1,16 @@
-import {pathToFileURL} from 'url';
+import {dirname} from 'path';
+import {fileURLToPath, pathToFileURL} from 'url';
 import {getWebTestRunnerConfigWithCoveragePercent} from 'virmator/base-configs/base-web-test-runner.mjs';
+
+const baseConfig = getWebTestRunnerConfigWithCoveragePercent({
+    coveragePercent: 0,
+    packageRootDirPath: dirname(dirname(fileURLToPath(import.meta.url))),
+    extraScreenshotOptions: {},
+});
 
 /** @type {import('@web/test-runner').TestRunnerConfig} */
 const webTestRunnerConfig = {
-    ...getWebTestRunnerConfigWithCoveragePercent(0),
+    ...baseConfig,
 };
 
 export default webTestRunnerConfig;
