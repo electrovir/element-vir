05969ad19329f867dab1bf72659938d70ab44934
electrovir <electrovir@users.noreply.github.com>
2023-05-10 00:29:47 +0000

    clear inputs that aren't set

--- src/declarative-element/properties/assign-inputs.ts
+++ src/declarative-element/properties/assign-inputs.ts
@@ -5,23 +5,35 @@ import {DeclarativeElement} from '../declarative-element';
 export function assignInputs(element: Element, inputs: object): void {
     const instanceState = (element as Partial<DeclarativeElement>).instanceState;
 
-    getObjectTypedKeys(inputs).forEach((key) => {
-        if (instanceState && key in instanceState) {
+    getObjectTypedKeys(inputs).forEach((newInputKey) => {
+        if (instanceState && newInputKey in instanceState) {
             throw new Error(
-                `Cannot set input '${key}' on '${element.tagName}'. '${element.tagName}' already has a state property with the same name.`,
+                `Cannot set input '${newInputKey}' on '${element.tagName}'. '${element.tagName}' already has a state property with the same name.`,
             );
         }
         /**
          * No need to check if it's already a property or not, as the property function already
          * makes that check.
          */
-        property()(element, key);
+        property()(element, newInputKey);
         if ('instanceInputs' in element) {
-            (element.instanceInputs as DeclarativeElement['instanceInputs'])[key] = inputs[key];
+            (element.instanceInputs as DeclarativeElement['instanceInputs'])[newInputKey] =
+                inputs[newInputKey];
         } else {
-            element[key] = inputs[key];
+            element[newInputKey] = inputs[newInputKey];
         }
     });
+
+    /** Wipe out all inputs that weren't set to undefined (as expected) */
+    if ('instanceInputs' in element) {
+        getObjectTypedKeys(element.instanceInputs).forEach((existingKey) => {
+            if (!(existingKey in inputs)) {
+                (element.instanceInputs as DeclarativeElement['instanceInputs'])[existingKey] =
+                    undefined;
+            }
+        });
+    }
+
     markInputsAsHavingBeenSet(element);
 }
 
