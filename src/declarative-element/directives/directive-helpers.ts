ccaef6c948ee662a2149683d216b8172a6bf2fd0
electrovir <electrovir@users.noreply.github.com>
2023-06-16 16:15:49 +0000

    add host tag name to directive helper error message
    GitHub: resolves #17

--- src/declarative-element/directives/directive-helpers.ts
+++ src/declarative-element/directives/directive-helpers.ts
@@ -1,4 +1,4 @@
-import {ElementPartInfo, PartInfo, PartType} from 'lit/directive.js';
+import {ChildPart, ElementPartInfo, PartInfo, PartType} from 'lit/directive.js';
 
 /** For some reason these aren't defined in lit's types already. */
 export type ExtraPartInfoProperties = {
@@ -16,14 +16,29 @@ export function extractElement(partInfo: PartInfo, directiveName: string): Eleme
     return element;
 }
 
+function getPartHostTagName(partInfo: PartInfo): string | undefined {
+    try {
+        const tagName = ((partInfo as ChildPart).options!.host as Element)!.tagName.toLowerCase();
+
+        return tagName;
+    } catch (error) {
+        return undefined;
+    }
+}
+
 export function assertIsElementPartInfo(
     partInfo: PartInfo,
     directiveName: string,
 ): asserts partInfo is ElementPartInfo & ExtraPartInfoProperties {
+    const hostTagName = getPartHostTagName(partInfo);
+    const hostTagMessage = hostTagName ? `: found in ${hostTagName}` : '';
+
     if (partInfo.type !== PartType.ELEMENT) {
-        throw new Error(`${directiveName} directive can only be attached directly to an element.`);
+        throw new Error(
+            `${directiveName} directive can only be attached directly to an element${hostTagMessage}.`,
+        );
     }
     if (!(partInfo as ElementPartInfo & ExtraPartInfoProperties).element) {
-        throw new Error(`${directiveName} directive found no element.`);
+        throw new Error(`${directiveName} directive found no element${hostTagMessage}.`);
     }
 }
