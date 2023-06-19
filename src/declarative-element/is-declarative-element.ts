d1cc74f1ecd81642fa4c0330c85703d4b5fd180a
electrovir <electrovir@users.noreply.github.com>
2023-06-19 13:34:50 +0000

    add jsdoc comment to isDeclarativeElement and assertsIsDeclarativeElement

--- src/declarative-element/is-declarative-element.ts
+++ src/declarative-element/is-declarative-element.ts
@@ -1,6 +1,10 @@
 import {typedHasProperties, typedHasProperty} from '@augment-vir/common';
 import {DeclarativeElement} from './declarative-element';
 
+/**
+ * Checks if the input is an instance of a DeclarativeElement, the super class of all custom
+ * elements defined with element-vir.
+ */
 export function isDeclarativeElement(input: unknown): input is DeclarativeElement {
     const markerProperties: ReadonlyArray<keyof DeclarativeElement> = [
         'instanceInputs',
@@ -9,7 +13,10 @@ export function isDeclarativeElement(input: unknown): input is DeclarativeElemen
     ];
     return typedHasProperties(input, markerProperties);
 }
-
+/**
+ * Checks if the input is an instance of a DeclarativeElement, the super class of all custom
+ * elements defined with element-vir.
+ */
 export function assertsIsDeclarativeElement(input: unknown): asserts input is DeclarativeElement {
     if (!isDeclarativeElement(input)) {
         console.error({notDeclarativeElement: input});
