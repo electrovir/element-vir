502151c269949ea2d5133db509718addfee18abc
electrovir <electrovir@users.noreply.github.com>
2023-11-02 00:40:31 +0000

    update deps

--- configs/ncu.config.ts
+++ configs/ncu.config.ts
@@ -6,7 +6,13 @@ export const ncuConfig: RunOptions = {
     // exclude these
     reject: [
         ...baseNcuConfig.reject,
+        /** Not ready for v3 yet. */
         'lit',
+        /**
+         * Version >1.0.0 can't handle the decorator we're using in declarative-element.ts. See
+         * https://github.com/modernweb-dev/web/blob/78226ea9b507c4cc236043d7fb930d28c3c1177c/packages/dev-server-esbuild/CHANGELOG.md#major-changes
+         */
+        '@web/dev-server-esbuild',
     ],
     // include only these
     filter: [],
