9cd17fef0099ec75a4d9e94997458ad5245119ec
electrovir <electrovir@users.noreply.github.com>
2024-01-13 17:53:12 +0000

    [major] rename IgnoreInputsNotBeenSetBeforeWarningSymbol to IgnoreUnsetInputsWarningSymbol

--- src/declarative-element/definition-options.ts
+++ src/declarative-element/definition-options.ts
@@ -1,18 +1,18 @@
-export const IgnoreInputsNotBeenSetBeforeWarningSymbol = Symbol(
+export const IgnoreUnsetInputsWarningSymbol = Symbol(
     'key for ignoring inputs not having been set yet',
 );
 
 export type DeclarativeElementDefinitionOptions = {
     /** For internal use only. */
-    [IgnoreInputsNotBeenSetBeforeWarningSymbol]: boolean;
+    [IgnoreUnsetInputsWarningSymbol]: boolean;
     /**
      * If set to true, state property keys will be allowed to change at run time. Turning this on
-     * will greatly reduce type safety and should be avoided at all costs.
+     * will greatly reduce type safety and should be avoided.
      */
     allowPolymorphicState: boolean;
 };
 
 export const defaultDeclarativeElementDefinitionOptions: DeclarativeElementDefinitionOptions = {
-    [IgnoreInputsNotBeenSetBeforeWarningSymbol]: true,
+    [IgnoreUnsetInputsWarningSymbol]: true,
     allowPolymorphicState: false,
 };
