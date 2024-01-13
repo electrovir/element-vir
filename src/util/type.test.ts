5d67709a16d35d5f9249ee8ed196efe114b07d93
electrovir <electrovir@users.noreply.github.com>
2024-01-13 00:29:59 +0000

    [dev] remove unused RequireNonVoidReturn, RequireSyncReturn, PickAndBlockOthers, ensureInstanceOf

--- src/util/type.test.ts
+++ src/util/type.test.ts
@@ -1,5 +1,4 @@
-import {assert} from '@open-wc/testing';
-import {ensureInstanceOf, NonEmptyString} from './type';
+import {NonEmptyString} from './type';
 
 describe('nonEmptyString', () => {
     it('should ensure type safety', () => {
@@ -12,21 +11,5 @@ describe('nonEmptyString', () => {
         // @ts-expect-error
         const nonSomethingString: NonEmptyString<'something'> = 'derp';
         const somethingString: NonEmptyString<'something'> = 'something';
-    });
-});
-
-describe(ensureInstanceOf.name, () => {
-    it('should propagate types correctly', () => {
-        const testObject: unknown = new RegExp('');
-
-        ensureInstanceOf(testObject, RegExp);
-        // this should not throw type errors
-        testObject.exec('');
-    });
-
-    it('should throw an error when the instance is wrong', () => {
-        const testObject: unknown = '';
-
-        assert.throws(() => ensureInstanceOf(testObject, RegExp), TypeError);
     });
 });
