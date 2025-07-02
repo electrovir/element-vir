b3935d2f35fa5abe4556c17e8aaea8595dac7882
electrovir <electrovir@users.noreply.github.com>
2025-07-02 20:29:41 +0000

    [patch] fix StatusWarning24Icon colors

--- packages/vira/src/icons/icon-svgs/status-warning-24.icon.ts
+++ packages/vira/src/icons/icon-svgs/status-warning-24.icon.ts
@@ -1,4 +1,5 @@
 import {html} from 'element-vir';
+import {viraIconCssVars} from '../icon-css-vars.js';
 import {defineIcon} from '../icon-svg.js';
 
 /**
@@ -12,12 +13,28 @@ export const StatusWarning24Icon = defineIcon({
     name: 'StatusWarning24Icon',
     svgTemplate: html`
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-            <circle cx="12" cy="12" r="9" style="fill:none;stroke:#000;stroke-width:1px" />
+            <circle
+                cx="12"
+                cy="12"
+                r="9"
+                fill=${viraIconCssVars['vira-icon-fill-color'].value}
+                stroke=${viraIconCssVars['vira-icon-stroke-color'].value}
+                stroke-width=${viraIconCssVars['vira-icon-stroke-width'].value}
+            />
             <path
                 d="m12 14 .2-7h-.4l.2 7Z"
-                style="stroke:#000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:2"
+                fill="none"
+                stroke=${viraIconCssVars['vira-icon-stroke-color'].value}
+                stroke-width=${viraIconCssVars['vira-icon-stroke-width'].value}
             />
-            <circle cx="12" cy="16" r="1" />
+            <circle
+                cx="12"
+                cy="16"
+                r="1"
+                fill=${viraIconCssVars['vira-icon-stroke-color'].value}
+                stroke=${viraIconCssVars['vira-icon-stroke-color'].value}
+                stroke-width="calc(${viraIconCssVars['vira-icon-stroke-width'].value} - 1px)"
+            />
         </svg>
     `,
 });
