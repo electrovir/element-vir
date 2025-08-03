bc0117df3a3594afcbf8d19bae185b7aaa2ff870
electrovir <electrovir@users.noreply.github.com>
2025-08-03 03:11:33 +0000

    [minor] add listenToEnter

--- packages/element-vir/src/declarative-element/directives/listen-to-activate.ts
+++ packages/element-vir/src/declarative-element/directives/listen-to-activate.ts
@@ -2,7 +2,8 @@ import {type MaybePromise} from '@augment-vir/common';
 import {listen} from './listen.directive.js';
 
 /**
- * Listens to enter, return, and space key hits on an element.
+ * Listens to enter, return, and space key hits on an element. Similar to {@link listenToEnter} but
+ * includes space.
  *
  * @category Directives
  */
@@ -11,6 +12,24 @@ export function listenToActivate(callback: () => MaybePromise<void>) {
         const key = event.code.toLowerCase();
 
         if (key.includes('enter') || key.includes('return') || key === 'space') {
+            event.stopImmediatePropagation();
+            event.preventDefault();
+            await callback();
+        }
+    });
+}
+
+/**
+ * Listens to enter and return key hits on an element. Similar to {@link listenToActivate} but
+ * doesn't include space.
+ *
+ * @category Directives
+ */
+export function listenToEnter(callback: () => MaybePromise<void>) {
+    return listen('keydown', async (event) => {
+        const key = event.code.toLowerCase();
+
+        if (key.includes('enter') || key.includes('return')) {
             event.stopImmediatePropagation();
             event.preventDefault();
             await callback();
